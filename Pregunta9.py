0de2554e8507d0b785ea20bbd5d8bac52ecaf7b1
Pedro Francisco Jacinto Abad <162522759+PedroFJAbad@users.noreply.github.com>
2024-03-07 06:38:01 +0000

    Solucion problema 9

---
+++ Pregunta9.py
@@ -0,0 +1,8 @@
+# Definir la lista original
+lista_original = ['Di', 'buen', 'día', 'a', 'papa']
+
+# Invertir la lista original
+lista_invertida = list(reversed(lista_original))
+
+# Mostrar la lista invertida
+print(lista_invertida)
